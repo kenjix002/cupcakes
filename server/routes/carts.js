d25554874cbeb302e7803b56ff5fd57c07b02bda
Steven <steven@gluecktech.com>
2021-05-17 22:47:31 +0800

    initial commit

---
+++ server/routes/carts.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+let Cart = require('../models/cart.model');
+// for user cart
+
+
+
+module.exports = router;
---
+++ server/routes/products.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+let Product = require('../models/product.model');
+//  for admin product CRUD
+
+
+
+module.exports = router;
